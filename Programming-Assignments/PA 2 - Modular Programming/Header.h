688424b74eb4b5707f6b774f5de68aaf5afcc89b
Mark Shinozaki <44478527+MarkShinozaki@users.noreply.github.com>
2021-10-26 11:41:54 -0700

    Create Header.h

---
+++ Programming-Assignments/PA 2 - Modular Programming/Header.h
@@ -0,0 +1,8 @@
+// function declerations
+char character_encryption(char plaintext_character, int shift_int);
+int calculate_series_resistance(int r1, int r2, int r3);
+double general_equation(int a, double x, double y, double z);
+double calculate_total_sales_tax(double sales_tax_rate, double item_cost);
+double calculate_volume_pyramid(double l, double w, double h);
+double distance_positions(double x1, double y1, double x2, double y2);
+float total_parallel_resistance(int r1, int r2, int r3);
